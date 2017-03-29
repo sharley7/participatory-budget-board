c1bdf7e44afa26092ab562883034834349c13c65
Siobhan <siobhan.harley@gmail.com>
2017-03-29 10:14:22 -0400

    login post action:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,18 @@ class UsersController < ApplicationController
     else
       erb :'/users/login'
     end
-   end 
+   end
+
+   post '/login' do
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/proposals"
+    else
+      redirect to '/signup'
+    end
+  end
 
 
 
