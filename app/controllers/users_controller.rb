1c123a0c0cccf53ddc0729030b23f6178739b25a
Siobhan <siobhan.harley@gmail.com>
2017-03-29 10:09:57 -0400

    signup post action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,16 @@ class UsersController < ApplicationController
     end
   end
 
+  post 'signup' do
+    if params[:username] == "" || params[:email] == "" | params[:password] == "" || params[:neighborhood_id] == ""
+       redirect to '/signup'
+    else
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password], neighborhood_id: params[:neighborhood_id]
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/proposals'
+    end 
+  end
 
 
 
