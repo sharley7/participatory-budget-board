f5b2d29b7dd6b58157363d22ed24438b9c1efed6
Siobhan <siobhan.harley@gmail.com>
2017-03-29 17:01:20 -0400

    add get action for proposals

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,6 +1,12 @@
 class ProposalsController < ApplicationController
 
-
+   get '/proposals' do
+    if logged_in?
+     erb: '/proposals/proposals'
+   else
+     redirect to '/signup'
+     end
+   end 
 
 
 end
