edbf015af0f07db1e9dcc8f9403ed92edba0a7f1
Siobhan <siobhan.harley@gmail.com>
2017-03-30 21:34:58 -0500

    slug methods for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ has_many :proposals
 belongs_to :neighborhood
 
   has_secure_password
-end 
+
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
+  
+end
